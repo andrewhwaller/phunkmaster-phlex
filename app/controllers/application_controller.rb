7ba62125752522c1d9307fe2c2ae53fb2e8e8128
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-01-08 22:34:33 -0600

    Update forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   allow_browser versions: :modern
 end
